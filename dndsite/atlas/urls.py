101f14ce3f7350c44f5298333a11725844ce7ba7
Trevor Brennan <TrevorBrennan4@gmail.com>
2018-03-04 18:16:09 -0500

    Created basic Views for Atlas App
    Followed part 3 of the django tutorial to create a generic set of views
    for the atlas app

--- dndsite/atlas/urls.py
+++ dndsite/atlas/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 
 from . import views
 
+app_name = 'atlas'
 urlpatterns = [
+    # ex: /atlas/
     path('', views.index, name='index'),
+    # ex: /atlas/1
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /atlas/1/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex /atlas/5/vots/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
