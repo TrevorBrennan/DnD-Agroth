863a2ae18a9c30a8d95a20ab2212604d7348e9be
Trevor Brennan <TrevorBrennan4@gmail.com>
2018-06-24 19:27:10 -0400

    Allow for characters without users. Add __str__ for Permission.

--- dndsite/authorization/models.py
+++ dndsite/authorization/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 class PlayerCharacter(models.Model):
 
     name = models.CharField(max_length=256)
-    player = models.ForeignKey(User, on_delete=models.CASCADE, related_name='characters')
+    player = models.ForeignKey(User, on_delete=models.CASCADE, related_name='characters', null=True)
     is_gm = models.BooleanField(default=False)
 
     def __str__(self):
@@ -19,3 +19,10 @@ class Permissions(models.Model):
     gm_only = models.BooleanField(default=True)
     authorized_characters = models.ManyToManyField(PlayerCharacter)
 
+    def __str__(self):
+        output = ""
+        if self.gm_only:
+            output = "GM Only"
+        for character in self.authorized_characters.all():
+            output = "{}, {}".format(output, character.name)
+        return output
