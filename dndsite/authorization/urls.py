98b7199f6414c4a3ee5a076d12f6505c171a6b7e
Trevor Brennan <TrevorBrennan4@gmail.com>
2019-09-28 01:09:40 -0400

    Update urls.py
    Add missing / in campaign url.

--- dndsite/authorization/urls.py
+++ dndsite/authorization/urls.py
@@ -6,5 +6,5 @@ app_name = 'authorization'
 urlpatterns = [
     # ex: /authorization/1
     path('character/<int:pk>', views.set_character, name='set_character'),
-    path('campaign<int:pk>', views.set_campaign, name='set_campaign'),
+    path('campaign/<int:pk>', views.set_campaign, name='set_campaign'),
 ]
